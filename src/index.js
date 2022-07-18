6756692abccfab2022474ebe60720d862b085440
David <davidcornejo1@live.com>
2022-07-18 00:17:59 -0600

    Added redirect on root path

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./index.css";
 import Expenses from "./pages/expenses";
 import Manage from "./pages/manage";
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter basename="/react">
     <Routes>
-      <Route path="/" />
+      <Route path="/" element={<Navigate to="/expenses" />} />
       <Route path="/expenses" element={<Expenses />} />
       <Route path="/manage" element={<Manage />} />
     </Routes>
